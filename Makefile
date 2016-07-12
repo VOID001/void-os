2afa884fe93d38b04aa13b251532af677e093bbf
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2016-07-12 11:23:10 +0800

    The barebone tutorial can work on real machine now

---
+++ Makefile
@@ -0,0 +1,21 @@
+TARGET=i686-elf
+CC=$(TARGET)-gcc
+LD=$(TARGET)-ld
+AS=$(TARGET)-as
+CFLAGS=-ffreestanding -O2 -nostdlib -std=gnu99 -Wall -Wextra
+LDFLAGS=-lgcc -T linker.ld
+
+void-os.iso: kernel.bin grub.cfg
+	- mkdir -p isodir/boot/grub
+	- cp kernel.bin isodir/boot/kernel.bin
+	- cp grub.cfg isodir/boot/grub/grub.cfg
+	- grub-mkrescue -o void-os.iso isodir
+
+boot-qemu: void-os.iso
+	- qemu-system-i386 -cdrom void-os.iso
+
+.o: .c
+	$(CC) $(CFLAGS) -c $<
+
+kernel.bin: kernel.o boot.o
+	$(CC) $(CFLAGS) -o kernel.bin $(LDFLAGS) boot.o kernel.o
---
+++ boot.s
@@ -0,0 +1,79 @@
+# Declare constants for multiboot header( Used by grub)
+.set ALIGN, 1 << 0
+.set MEMINFO, 1 << 1
+.set FLAGS, ALIGN | MEMINFO
+.set MAGIC, 0x1BADB002
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+# Decalre a multiboot header, marks the program as a kernel
+# These are magic values documented in multiboot standard
+# bootloader will serach for this signature in the first 8KiB
+# of the kernel file, we use linker script to force the header
+# section to be within the first 8KiB of kernel file
+
+.section .multiboot
+.align   4
+.long    MAGIC
+.long    FLAGS
+.long    CHECKSUM
+
+# Set up a stack for the kernel, since grub does not provide
+# The setup of %esp, We manually create a small stack by
+# creating a symbol at the bottom of it, then allocating 16384
+# bytes for it, and finally creating a symbol at the top. The stack grows
+# downwards on x86. The stack is in its own section so it can be marked nobits
+# which means the kernel file is smaller because it does not contain an
+# uninitilaized stack
+
+.section .bootstrap_stack, "aw", @nobits
+
+stack_bottom:
+	.skip 16384 # 16KiB
+
+stack_top:
+
+# The linker script specifies _start as the entry point to the kernel
+# and the bootloader will jump to this position once the kernel has been
+# loaded It doesn't make sense to return from this function as the bootloader
+# is gone
+
+.section .text
+.global  _start
+.type    _start, @function
+
+_start:
+
+# The bootloader loaded us into 32-bit protected mode on a x86 machine
+# Interrupts, Paging is disabled, The process state is as defined in
+# the multiboot standard. The kernel has full control of the CPU.
+# The kernel can only make use of hardware features and any code provides
+# as part of itself. no <stdio.h> no printf, etc... Now we have the full
+# access to CPU
+
+# Setup the stack, it grow downwards on x86 machines, so make esp point
+# to stack_top(higher address)
+
+mov $stack_top, %esp
+
+# Here we should initialize crucial processor state before the high level
+# kernel is entered. It's best to minimize the early environment where
+# crucial features are offline. Note now the processor is not fully initialized
+# yet: Features such as floating point instructions and instruction set extensions
+# not init yet, GDT should be loaded here, Paging should be enabled here.
+# C++ features such as global constructors and exceptions will require runtime support
+# to work as well
+
+# Enter the high-level kernel
+
+call kernel_main
+
+cli
+
+# Halt the system forever after kernel return
+1:
+	hlt
+	jmp 1b
+
+# Set the size of the _start symbol to the current location '.' minus its start.
+# This is useful when debugging or when implementing call tracing.
+.size _start, . - _start
